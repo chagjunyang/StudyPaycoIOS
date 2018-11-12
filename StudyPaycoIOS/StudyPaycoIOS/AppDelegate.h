c9f75f158bd7a32e58b9bc489f2f476d8255bcd4
cjy <changjun.yang@nhnent.com>
2018-11-12 21:40:48 +0900

    init study project

---
+++ StudyPaycoIOS/StudyPaycoIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StudyPaycoIOS
+//
+//  Created by cjyang on 2018. 11. 12..
+//  Copyright © 2018년 NHNENT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StudyPaycoIOS/StudyPaycoIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StudyPaycoIOS
+//
+//  Created by cjyang on 2018. 11. 12..
+//  Copyright © 2018년 NHNENT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
